c7dbe9746808b0239e13eb31beaa9eccd88792da
jameswong3388 <jameswong@gmail.com>
2023-10-07 09:27:53 +0800

    1. Add new Task Api 2. Update Tasks model

---
+++ src/main/java/com/oodwj_assignment/APIs/Task.java
@@ -0,0 +1,157 @@
+package com.oodwj_assignment.APIs;
+
+import com.oodwj_assignment.Models.Tasks;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.UUID;
+
+public class Task {
+    private static final String FILE_NAME = "src/main/java/com/oodwj_assignment/Databases/tasks.txt";
+
+    public static Response<UUID> create(Tasks task) {
+        UUID taskId = UUID.randomUUID();
+        task.setTaskId(taskId);
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_NAME, true))) {
+            writer.println(task);
+
+            return Response.success("Task created successfully", taskId);
+        } catch (IOException e) {
+            return Response.failure("Failed to create task: " + e.getMessage());
+        }
+    }
+
+    public static Response<ArrayList<Tasks>> read(Map<String, Object> query) {
+        ArrayList<Tasks> tasks = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(";");
+                if (parts.length == 7) {
+                    UUID taskId = UUID.fromString(parts[0]);
+                    UUID runnerId = UUID.fromString(parts[1]);
+                    UUID orderId = UUID.fromString(parts[2]);
+                    Integer deliveryFee = Integer.parseInt(parts[3]);
+                    Tasks.taskStatus status = Tasks.taskStatus.valueOf(parts[4]);
+                    LocalDate updatedAt = LocalDate.parse(parts[5]);
+                    LocalDate createdAt = LocalDate.parse(parts[6]);
+
+                    tasks.add(new Tasks(taskId, runnerId, orderId, deliveryFee, status, updatedAt, createdAt));
+
+                }
+            }
+        } catch (IOException e) {
+            return Response.failure("Failed to read tasks: " + e.getMessage());
+        }
+
+        if (query.isEmpty()) {
+            return Response.success("Tasks read successfully", tasks);
+        }
+
+        ArrayList<Tasks> matchTasks = new ArrayList<>();
+
+        for (Tasks task : tasks) {
+            Response<Void> matchRes = match(query, task);
+
+            if (matchRes.isSuccess()) {
+                matchTasks.add(task);
+            }
+        }
+
+        return Response.success("Tasks read successfully", matchTasks);
+    }
+
+    public static Response<Void> update(Map<String, Object> query, Map<String, Object> newValue) {
+        Response<ArrayList<Tasks>> tasks = read(query);
+
+        if (tasks.isSuccess()) {
+            for (Tasks task : tasks.getData()) {
+                for (Map.Entry<String, Object> entry : newValue.entrySet()) {
+                    String attributeName = entry.getKey();
+                    Object expectedValue = entry.getValue();
+
+                    Response<Void> updateRes = task.setAttributeValue(attributeName, expectedValue);
+
+                    if (!updateRes.isSuccess()) {
+                        return Response.failure(updateRes.getMessage());
+                    }
+                }
+            }
+
+            Response<Void> saveRes = saveAllTasks(tasks.getData());
+
+            if (saveRes.isSuccess()) {
+                return Response.success("Tasks updated successfully");
+            } else {
+                return Response.failure(saveRes.getMessage());
+            }
+        } else {
+            return Response.failure(tasks.getMessage());
+        }
+    }
+
+    public static Response<Void> delete(Map<String, Object> query) {
+        Response<ArrayList<Tasks>> tasks = read(Map.of());
+
+        if (tasks.isSuccess()) {
+            for (Tasks task : tasks.getData()) {
+                Response<Void> matchRes = match(query, task);
+
+                if (matchRes.isSuccess()) {
+                    tasks.getData().remove(task);
+                    Response<Void> saveRes = saveAllTasks(tasks.getData());
+
+                    if (saveRes.isSuccess()) {
+                        return Response.success("Task deleted successfully");
+                    } else {
+                        return Response.failure(saveRes.getMessage());
+                    }
+                }
+            }
+            return Response.failure("Task not found");
+        } else {
+            return Response.failure(tasks.getMessage());
+        }
+    }
+
+    private static Response<Void> match(Map<String, Object> query, Tasks task) {
+        boolean match = true;
+
+        for (Map.Entry<String, Object> entry : query.entrySet()) {
+            String attributeName = entry.getKey();
+            Object expectedValue = entry.getValue();
+
+            Response<Object> actualValue = task.getAttributeValue(attributeName);
+
+            if (actualValue.isSuccess()) {
+                if (!actualValue.getData().equals(expectedValue)) {
+                    match = false;
+                    break;
+                }
+            } else {
+                return Response.failure(actualValue.getMessage());
+            }
+        }
+        if (match) {
+            return Response.success("Task matched successfully");
+        } else {
+            return Response.failure("Task not found");
+        }
+    }
+
+    private static Response<Void> saveAllTasks(ArrayList<Tasks> tasks) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_NAME))) {
+            for (Tasks task : tasks) {
+                writer.println(task.toString());
+            }
+        } catch (IOException e) {
+            return Response.failure(e.getMessage());
+        }
+
+        return Response.success("Tasks saved successfully");
+    }
+}
--- src/main/java/com/oodwj_assignment/Models/Tasks.java
+++ src/main/java/com/oodwj_assignment/Models/Tasks.java
@@ -1,16 +1,18 @@
 package com.oodwj_assignment.Models;
 
+import com.oodwj_assignment.APIs.Response;
+
 import java.time.LocalDate;
 import java.util.UUID;
 
 public class Tasks {
-    private final UUID taskId;
-    private final UUID runnerId; // may be null
-    private final UUID orderId;
-    private final Integer deliveryFee;
-    private final taskStatus status;
-    public final LocalDate updatedAt;
-    public final LocalDate createdAt;
+    private UUID taskId;
+    private UUID runnerId; // may be null
+    private UUID orderId;
+    private Integer deliveryFee;
+    private taskStatus status;
+    public LocalDate updatedAt;
+    public LocalDate createdAt;
 
     public Tasks(UUID taskId, UUID runnerId, UUID orderId, Integer deliveryFee, taskStatus status, LocalDate updatedAt, LocalDate createdAt) {
         this.taskId = taskId;
@@ -48,6 +50,103 @@ public class Tasks {
 
     public LocalDate getCreatedAt() {
         return createdAt;
+    }
+
+    public Response<Object> getAttributeValue(String attributeName) {
+        return switch (attributeName) {
+            case "taskId" -> Response.success("Task ID read successfully", getTaskId());
+            case "runnerId" -> Response.success("Runner ID read successfully", getRunnerId());
+            case "orderId" -> Response.success("Order ID read successfully", getOrderId());
+            case "deliveryFee" -> Response.success("Delivery Fee read successfully", getDeliveryFee());
+            case "status" -> Response.success("Status read successfully", getStatus());
+            case "updatedAt" -> Response.success("Updated At read successfully", getUpdatedAt());
+            case "createdAt" -> Response.success("Created At read successfully", getCreatedAt());
+            default -> Response.failure("No such attribute");
+        };
+    }
+
+    public void setTaskId(UUID taskId) {
+        this.taskId = taskId;
+    }
+
+    public void setRunnerId(UUID runnerId) {
+        this.runnerId = runnerId;
+    }
+
+    public void setOrderId(UUID orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setDeliveryFee(Integer deliveryFee) {
+        this.deliveryFee = deliveryFee;
+    }
+
+    public void setStatus(taskStatus status) {
+        this.status = status;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Response<Void> setAttributeValue(String attributeName, Object newValue) {
+        switch (attributeName) {
+            case "taskId":
+                if (newValue instanceof UUID) {
+                    setTaskId((UUID) newValue);
+                    return Response.success("Task ID updated successfully");
+                } else {
+                    return Response.failure("Task ID must be a UUID");
+                }
+            case "runnerId":
+                if (newValue instanceof UUID) {
+                    setRunnerId((UUID) newValue);
+                    return Response.success("Runner ID updated successfully");
+                } else {
+                    return Response.failure("Runner ID must be a UUID");
+                }
+            case "orderId":
+                if (newValue instanceof UUID) {
+                    setOrderId((UUID) newValue);
+                    return Response.success("Order ID updated successfully");
+                } else {
+                    return Response.failure("Order ID must be a UUID");
+                }
+            case "deliveryFee":
+                if (newValue instanceof Integer) {
+                    setDeliveryFee((Integer) newValue);
+                    return Response.success("Delivery Fee updated successfully");
+                } else {
+                    return Response.failure("Delivery Fee must be an Integer");
+                }
+            case "status":
+                if (newValue instanceof taskStatus) {
+                    setStatus((taskStatus) newValue);
+                    return Response.success("Status updated successfully");
+                } else {
+                    return Response.failure("Status must be a taskStatus");
+                }
+            case "updatedAt":
+                if (newValue instanceof LocalDate) {
+                    setUpdatedAt((LocalDate) newValue);
+                    return Response.success("Updated At updated successfully");
+                } else {
+                    return Response.failure("Updated At must be a LocalDate");
+                }
+            case "createdAt":
+                if (newValue instanceof LocalDate) {
+                    setCreatedAt((LocalDate) newValue);
+                    return Response.success("Created At updated successfully");
+                } else {
+                    return Response.failure("Created At must be a LocalDate");
+                }
+            default:
+                return Response.failure("No such attribute");
+        }
     }
 
     public enum taskStatus {
